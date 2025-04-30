8ff027be763dff97e4e75d056aaea80365104da6
adityaraze <adityamodanwal293@gmail.com>
2025-04-30 15:24:06 +0530

    lol

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,65 +1,82 @@
+"use client";
+
 import Image from "next/image";
-import React from "react";
-import av from "../public/ai-avatar.png";
-import av1 from "../public/user-avatar.png";
-import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
+
 import { cn } from "@/lib/utils";
 
-enum CallStatus{
-INACTIVE = 'INACTIVE',
-CONNECTING = 'CONNECTING',
-ACTIVE = 'ACTIVE',
-FINISHED = 'FINISHED',
+enum CallStatus {
+  INACTIVE = "INACTIVE",
+  CONNECTING = "CONNECTING",
+  ACTIVE = "ACTIVE",
+  FINISHED = "FINISHED",
 }
 
-const Agent = ({ userName }: AgentProps) => {
-  const callStatus = CallStatus.FINISHED;
+
+const Agent = ({
+  userName,
+}: AgentProps) => {
+  const router = useRouter();
+  const callStatus =  CallStatus.ACTIVE
   const isSpeaking = true;
+
   const messages = [
-    'What is your name ?',
-    'My name is Aditya Modanwal'
+    'Hello Everyone',
+    'My name is john doe'
   ];
+
   const lastMessage = messages[messages.length-1];
+
   return (
     <>
       <div className="call-view">
+        {/* AI Interviewer Card */}
         <div className="card-interviewer">
           <div className="avatar">
             <Image
-              src={av}
-              alt="vapi"
+              src="/ai-avatar.png"
+              alt="profile-image"
               width={65}
               height={54}
               className="object-cover"
             />
-            {isSpeaking && <span className="animate-speak"></span>}
+            {isSpeaking && <span className="animate-speak" />}
           </div>
           <h3>AI Interviewer</h3>
         </div>
+
+        {/* User Profile Card */}
         <div className="card-border">
           <div className="card-content">
             <Image
-              src={av1}
-              alt="user avatar"
-              width={540}
-              height={540}
+              src="/user-avatar.png"
+              alt="profile-image"
+              width={539}
+              height={539}
               className="rounded-full object-cover size-[120px]"
             />
             <h3>{userName}</h3>
           </div>
         </div>
       </div>
+
       {messages.length > 0 && (
         <div className="transcript-border">
           <div className="transcript">
-            <p key={lastMessage} className={cn('transition-opacity duration-500 opacity-0','animate-fadeIn opacity-100')}>
+            <p key={lastMessage}
+              className={cn(
+                "transition-opacity duration-500 opacity-0",
+                "animate-fadeIn opacity-100"
+              )}
+            >
               {lastMessage}
             </p>
           </div>
         </div>
       )}
+
       <div className="w-full flex justify-center">
-      {callStatus !== "ACTIVE" ? (
+        {callStatus !== "ACTIVE" ? (
           <button className="relative btn-call">
             <span
               className={cn(
